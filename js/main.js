30508390eafe000ce15b7775773bbd379c4b92c9
Mohamad Salimi <mohamad.salimi@hotmail.com>
2021-10-31 14:29:51 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
+let form = document.querySelector("form");
+let email = document.getElementById("email");
 
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let emailValue = email.value.trim();
+    if (emailValue === "" || !validateEmail(emailValue)) {
+        form.classList.add("error");
+    } else {
+        form.classList.remove("error");
+    }
+})
+
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
